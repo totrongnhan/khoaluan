c62e3cfbb5e29a6925f3619bd2aefcb812c633c7
nhan <tn>
2023-06-04 19:54:13 +0700

    admin

---
+++ DanhGi/danhgia.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Trang đánh giá</title>
+        <link rel="shortcut icon" href="../assets/img/logotaydo.png" >
+        <link rel="shortcut icon" href="../assets/img/logotaydo.png" >
+        <link href="assets/css/bootstrap.css" rel="stylesheet" />
+        <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <link href="assets/css/style.css" rel="stylesheet" />
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.16.15/dist/css/uikit.min.css" />
+
+    </head>
+    <body>
+        <?php
+        include('../include/header.php');
+        ?>
+        <table class="uk-table uk-table-justify uk-table-divider">
+    <thead>
+        <tr>
+            <th class="uk-width-small">Mã đánh giá</th>
+            <th>Tên đánh giá</th>
+            <th>Tham gia</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>DG1</td>
+            <td>Cơ sở vật chất</td>
+            <td> <a href="danhgiacsvc/csvc.php" class="uk-button uk-button-default" data-hover="dropdown">Khảo sát</a></td>
+            <a href="csvc.php"></a>
+        </tr>
+        <tr>
+            <td>DG2</td>
+            <td>Chương trình đào tạo</td>
+           <td> <a href="danhgiactdt/ctdt.php" class="uk-button uk-button-default" data-hover="dropdown">Khảo sát</a></td>
+            
+        </tr>
+    </tbody>
+</table>
+  </tbody>
+</table>
+        <script src="assets/js/jquery-1.10.2.js"></script>
+        <script src="assets/js/bootstrap.js"></script>
+        <script src="assets/js/custom.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.15/dist/js/uikit.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.15/dist/js/uikit-icons.min.js"></script>
+    </body>
+
+</html>
